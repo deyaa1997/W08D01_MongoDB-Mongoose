5a7650dbc88fc8332a44e90f8d834641c7c41885
deyaa1997 <deyah.mosa@gmail.com>
2021-05-16 13:20:44 +0300

    Practice 1 And 2

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const todoModel = require("./schema");
 const db = require("./db");
+const { Mongoose } = require("mongoose");
 
 const app = express();
 app.use(express.json());
 
-
 app.get("/todos", (req, res) => {});
-app.post("/create/todo", (req, res) => {});
+
+app.post("/create/todo", (req, res) => {
+  const { task, description, deadline, isCompleted, priority } = req.body;
+
+  const todo = new todoModel({
+    task,
+    description,
+    deadline,
+    isCompleted,
+    priority,
+  });
+
+  todo
+    .save()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
 app.put("/update/todo", (req, res) => {});
+
 app.delete("/delete/todo", (req, res) => {});
 
 const port = 3000;

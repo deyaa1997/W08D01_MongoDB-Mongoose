3de91994be740ec314d282e587c3a977664c5c28
deyaa1997 <deyah.mosa@gmail.com>
2021-05-16 12:33:14 +0300

    Pulse Check 2

--- db.js
+++ db.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+// connecting mongoose
+mongoose.connect("mongodb://localhost:27017/DB_Name", options).then(
+  () => {
+    console.log("DB Ready To Use");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
